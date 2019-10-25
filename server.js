0b60fe0742fac653941ea766bfe5b68a46970c78
Ethan Jurman <ethanjurman@gmail.com>
2019-10-25 01:23:56 -0400

    working send keys

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express')
+const express = require('express');
 const ks = require('node-key-sender');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/key/:code', (req, res) => {
-  console.log('key:', req.params.code)
-  ks.sendKey(req.params.code)
-  res.send('ok')
-})
+  console.log('key:', +req.params.code + 32);
+  ks.sendKey(String.fromCharCode(+req.params.code + 32));
+  res.send('ok');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
